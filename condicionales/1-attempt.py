08d42aeccd0295fe8c8c491f4be79b09460a3938
Dupertroll <laqm0103@gmail.com>
2021-10-25 15:52:08 -0500

    Constantes

--- condicionales/1-attempt.py
+++ condicionales/1-attempt.py
@@ -3,18 +3,18 @@ print("1 = pizza normal");
 print("2 = pizza vegetariana");
 preguntar = int(input("¿Cual le gustaria ordenar señor? "));
 
-if (preguntar >= 3):
-    print("esa orden no existe por ahora, vuelva pronto.");
+
 if(preguntar == 1):
     pizza1 = input("Esta pizza contiene peperoni, queso y jamon, asi esta bien? ");
     if(pizza1 == "si"):
         print("Esta bien, su orden ya esta en camino");
     else:
         print("Lamentamos no cumplir sus expectativas, fue un placer hablar con usted, tenga un buen dia.");
-if(preguntar == 2):
+elif(preguntar == 2):
     pizza2 = input("Esta pizza contiene tofu, queso y pimienta, ¿asi esta bien? ");
     if(pizza2 == "si"):
         print("Esta bien, su orden ya esta en camino");
     else:
         print("Lamentamos no cumplir sus expectativas, fue un placer hablar con usted");
-        
+else:
+    print("No tenemos esa orden por ahora, vuelva pronto");
---
+++ constantes/1-attempt.py
@@ -0,0 +1,13 @@
+NOMBRE1 = "Me encanta el helado";
+
+FRASE1 = "de vainilla y";
+
+nombre1 = "de chocolate";
+
+frase1 = "aunque el de fresa tambien me gusta mucho";
+
+print(NOMBRE1, FRASE1, nombre1, frase1);
+
+PREGUNTAR = input("¿a ti que te gusta mas el helado de vainilla o de chocholate? ");
+
+print("esta bien, respeto tus gustos y si a ti te gusta mas ese helado esta bien.");
