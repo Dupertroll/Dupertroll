7251b727d5b9270feee50994fc67aeeb77b8aa8e
Dupertroll <laqm0103@gmail.com>
2021-10-27 12:29:29 -0500

    Se les agrego el modulo de tiempo

--- constantes/1-attempt.py
+++ constantes/1-attempt.py
@@ -1,3 +1,5 @@
+import time;
+
 NOMBRE1 = "Me encanta el helado";
 
 FRASE1 = "de vainilla y";
@@ -9,5 +11,5 @@ frase1 = "aunque el de fresa tambien me gusta mucho";
 print(NOMBRE1, FRASE1, nombre1, frase1);
 
 PREGUNTAR = input("¿a ti que te gusta mas el helado de vainilla o de chocholate? ");
-
+time.sleep(1)
 print("esta bien, respeto tus gustos y si a ti te gusta mas ese helado esta bien.");
---
+++ variables/1-attempt.py
@@ -0,0 +1,7 @@
+introducirEdad = int(input("introduce tu edad: "));
+print(introducirEdad);
+
+if(introducirEdad < 20):
+    print("Felicidades, aun tienes mucho por vivir"); 
+else:
+    print("ya tas viejo pa");
